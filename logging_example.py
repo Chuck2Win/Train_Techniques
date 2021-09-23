6540e43926fb8e8445caf36f3f0f9fb038924360
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-09-23 11:02:24 +0900

    Update logging_example.py

--- logging_example.py
+++ logging_example.py
@@ -2,11 +2,10 @@ import logging
 logger = logging.getLogger('logger') # 적지 않으면 root로 생성
 
 # 2. logging level 지정 - 기본 level Warning
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
-# 3. logging formatting 설정 - 문자열 format과 유사
-formatter = logging.Formatter(\
-'LOG##LOGSAMPLE##%(levelname)s##%(asctime)s##%(message)s >> @@file::%(filename)s@@line::%(lineno)s')
+# 3. logging formatting 설정 - 문자열 format과 유사 - 시간, logging 이름, level - messages
+formatter = logging.Formatter('[%(asctime)s][%(name)s][%(levelname)s] >> %(message)s')
 
 # 4. handler : log message를 지정된 대상으로 전달하는 역할.
 # SteamHandler : steam(terminal 같은 console 창)에 log message를 보냄
@@ -18,6 +17,8 @@ steam_handler.setFormatter(formatter)
 # logger instance에 handler 삽입
 logger.addHandler(stream_handler)
 
-file_handler = logging.FileHandler()
+file_handler = logging.FileHandler('log.txt', encoding='utf-8')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+
+# 기록하면된다.
