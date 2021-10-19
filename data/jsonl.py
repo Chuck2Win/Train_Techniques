7d2284399ea416846d1a09f92fd00aa893f37f23
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-10-19 19:03:34 +0900

    Update jsonl.py

--- data/jsonl.py
+++ data/jsonl.py
@@ -4,3 +4,8 @@ data = []
 for i in f:
   data.append(i) # 이러면 json이 추가될 것임
   
+import json 
+row_data = [{"id":"a","title":"higuys","genre":"drama"}, {"id":"b","title":"make_me_happy","genre":"action"}] 
+with open("make_jsonl.jsonl" , encoding= "utf-8",mode="w") as file: 
+  for i in row_data: 
+    file.write(json.dumps(i) + "\n")
